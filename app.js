831c5ce8f593c4454a9b97bbd7b0c88d76e41ed4
Ricardo <rjfloresc14@gmail.com>
2023-11-08 14:57:40 -0600

    Js entrega 1

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// mi funcion y mis 3 variables
 function calcularCuota() {
     var monto = parseFloat(document.getElementById("monto").value);
     var plazo = parseInt(document.getElementById("plazo").value);
@@ -11,12 +12,23 @@ function calcularCuota() {
     var tasaMensual = tasa / 12;
     var cuota = monto * (tasaMensual / (1 - Math.pow(1 + tasaMensual, -plazo)));
     
-    document.getElementById("resultado").innerHTML = "Cuota Mensual: $" + cuota.toFixed(2);
+    var resultadoElement = document.getElementById("resultado");
+    resultadoElement.innerHTML = "Cuota Mensual: $" + cuota.toFixed(2);
+// la idea con de la siguiente formula, es asesorar al cliente, haciendole elegir un plazo que le convenga.
+    if (cuota > monto * 0.3) {
+        resultadoElement.innerHTML += "<br>La cuota es mayor del 30% del monto, ¡considere una tasa o plazo diferente!";
+    } else if (cuota < monto * 0.1) {
+        resultadoElement.innerHTML += "<br>La cuota es menor del 10% del monto, ¡puede pagar más rápido!";
+    }
 }
-
+// botoncito para limpiar no puede faltar
 function limpiarCampos() {
     document.getElementById("monto").value = "";
     document.getElementById("plazo").value = "";
     document.getElementById("tasa").value = "";
     document.getElementById("resultado").innerHTML = "";
 }
+
+
+
+
